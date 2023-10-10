e2221205cceebe68593dc5fd9219407ce0fbd7cf
whymezi <128997288+whymezi@users.noreply.github.com>
2023-10-10 19:46:19 +0600

    Add files via upload

---
+++ project_program_for_students/js/home_works.js
@@ -0,0 +1,34 @@
+//GMAIL
+
+const input = document.querySelector('#gmail_input')
+const checker = document.querySelector('#gmail_button')
+const result = document.querySelector('#gmail_result')
+
+const regExp = /^[a-z/\d{2, 4}]+[@\"]+[gmail.com]+$/ig
+
+checker.onclick = () => {
+    if(regExp.test(input.value)){
+        result.innerHTML = 'true'
+        result.style.color = 'green'
+    }else{
+        result.innerHTML = 'false'
+        result.style.color = 'red'
+    }
+}
+
+
+//BALL
+
+const ball = document.querySelector('.child_block')
+
+let goBall1 = 0
+let goBall2 = 0
+
+const animation = () => {
+    if(goBall1 < 90){
+        goBall1+=1
+        ball.style.left = `${goBall1}%`
+        setTimeout(animation, 10)
+    }
+}
+animation()
